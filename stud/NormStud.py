4b572a3847cda5102af5a5095a004198faeae158
basra4ka <evgeha_ivanov_01@bk.ru>
2024-03-20 13:24:32 +0300

    s

--- stud/NormStud.py
+++ stud/NormStud.py
@@ -1,5 +1,6 @@
 import math
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 
@@ -9,32 +10,31 @@ def count_n(tbl):
         n += tbl[k]
     return n
 
+
 def count_Xv(tbl, n):
     sum = 0
     for k in tbl.keys():
         sum += tbl[k] * k
 
-    return sum/n
+    return sum / n
+
 
 def count_por(tbl, n, Xv, p):
     sum = 0
     for k in tbl.keys():
         sum += tbl[k] * (math.pow((k - Xv), p))
 
-    return sum/n
-
+    return sum / n
 
 
 df = pd.read_csv("gr-1.csv")
 
-table = {2:0, 3:0, 4:0, 5:0}
+table = {2: 0, 3: 0, 4: 0, 5: 0}
 
 for i, r in df.iterrows():
     table[int(r['mark'])] += 1
 
-
 n = count_n(table)
-
 
 Xv = count_Xv(table, n)
 Dv = count_por(table, n, Xv, 2)
@@ -43,4 +43,6 @@ Sv = math.sqrt(Dv)
 A = count_por(table, n, Xv, 3) / math.pow(Sv, 3)
 E = count_por(table, n, Xv, 4) / math.pow(Sv, 4) - 3
 
-
+plt.hist(df['mark'])
+sns.displot(df, kde=True, bins=4)
+plt.show()
